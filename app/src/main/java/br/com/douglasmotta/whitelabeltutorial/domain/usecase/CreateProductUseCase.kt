63bf4b24c6bce5b902cc34ab5ade63c43347de1d
jvsena42 <jvsena16@gmail.com>
2022-01-01 10:50:08 -0300

    feat: datasource

--- app/src/main/java/br/com/douglasmotta/whitelabeltutorial/domain/usecase/CreateProductUseCase.kt
+++ app/src/main/java/br/com/douglasmotta/whitelabeltutorial/domain/usecase/CreateProductUseCase.kt
@@ -1,8 +1,8 @@
 package br.com.douglasmotta.whitelabeltutorial.domain.usecase
-
+import android.net.Uri
 import br.com.douglasmotta.whitelabeltutorial.domain.model.Product
 
-interface CreateProductUserCase {
+interface CreateProductUseCase {
 
     suspend operator fun invoke(description: String, imageUri: Uri) : Product
 }
--- app/src/main/java/br/com/douglasmotta/whitelabeltutorial/domain/usecase/UploadProductImageUseCase.kt
+++ app/src/main/java/br/com/douglasmotta/whitelabeltutorial/domain/usecase/UploadProductImageUseCase.kt
@@ -1,5 +1,7 @@
 package br.com.douglasmotta.whitelabeltutorial.domain.usecase
 
+import android.net.Uri
+
 interface UploadProductImageUseCase {
 
     suspend operator fun invoke(imageUri: Uri) : String
