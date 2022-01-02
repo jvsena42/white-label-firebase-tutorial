cf03f252a083f52f3ca8f3ee7ccc2905f62d2687
jvsena42 <jvsena16@gmail.com>
2022-01-02 10:54:21 -0300

    feat: uploadProductImage

--- app/src/main/java/br/com/douglasmotta/whitelabeltutorial/data/FirebaseProductDataSource.kt
+++ app/src/main/java/br/com/douglasmotta/whitelabeltutorial/data/FirebaseProductDataSource.kt
@@ -5,8 +5,10 @@ import br.com.douglasmotta.whitelabeltutorial.BuildConfig
 import br.com.douglasmotta.whitelabeltutorial.domain.model.Product
 import br.com.douglasmotta.whitelabeltutorial.util.COLLECTION_PRODUCTS
 import br.com.douglasmotta.whitelabeltutorial.util.COLLECTION_ROOT
+import br.com.douglasmotta.whitelabeltutorial.util.STORAGE_IMAGES
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.storage.FirebaseStorage
+import java.util.*
 import kotlin.coroutines.suspendCoroutine
 
 class FirebaseProductDataSource(
@@ -18,10 +20,10 @@ class FirebaseProductDataSource(
         "$COLLECTION_ROOT/${BuildConfig.FIREBASE_FLAVOR_COLLECTION}/"
     )
 
-    private val storeReference = firebaseStorage.reference
+    private val storageReference = firebaseStorage.reference
 
     override suspend fun getProducts(): List<Product> {
-        return suspendCoroutine {  continuation ->
+        return suspendCoroutine { continuation ->
             val productsReference = documentReference.collection(COLLECTION_PRODUCTS)
 
             productsReference.get().addOnSuccessListener { documents ->
@@ -41,7 +43,22 @@ class FirebaseProductDataSource(
     }
 
     override suspend fun uploadProductImage(imageUri: Uri): String {
-        TODO("Not yet implemented")
+        return suspendCoroutine { continuation ->
+            val randomKey = UUID.randomUUID()
+            val childReference = storageReference.child(
+                "$STORAGE_IMAGES/${BuildConfig.FIREBASE_FLAVOR_COLLECTION}/$randomKey"
+            )
+            childReference.putFile(imageUri)
+                .addOnSuccessListener { taskSnapshot ->
+                    taskSnapshot.storage.downloadUrl.addOnSuccessListener { uri ->
+                        val path = uri.toString()
+                        continuation.resumeWith(Result.success(path))
+                    }
+            }
+                .addOnFailureListener { exception ->
+                continuation.resumeWith(Result.failure(exception))
+            }
+        }
     }
 
     override suspend fun createProduct(product: Product): Product {
